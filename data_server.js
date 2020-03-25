08052a8f34fd47ab6349ec9d1b4943c84f9f3ce8
steven <radishj@gmail.com>
2020-03-25 15:44:48 -0700

    some

--- data_server.js
+++ data_server.js
@@ -3,7 +3,11 @@ var bodyParser = require("body-parser");
 //var couchbase = require("couchbase");
 var cors = require('cors')
 var app = express();
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 /*
